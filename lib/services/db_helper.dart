0b8e3eb6483b569fca9836d074d49610ce86d2d2
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-05-11 20:11:54 +0500

    Add Database Helper

---
+++ lib/services/db_helper.dart
@@ -0,0 +1,40 @@
+import 'dart:io' as io;
+
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+import '../model/cart_model.dart';
+
+class DBHelper {
+  static Database? _db;
+
+  Future<Database?> get db async {
+    if (_db != null) {
+      return _db!;
+    }
+
+    _db = await initDatabase();
+    return _db!;
+  }
+
+  initDatabase() async {
+    io.Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, 'cart.db');
+
+    var db = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return db;
+  }
+
+  _onCreate(Database db, int version) async {
+    await db.execute(
+      'CREATE TABLE cart (id INTEGER PRIMARY KEY , productId VARCHAR UNIQUE,productName TEXT,initialPrice INTEGER, productPrice INTEGER , quantity INTEGER, unitTag TEXT , image TEXT )',
+    );
+  }
+
+  Future<CartModel> insert(CartModel cart) async {
+    var dbClient = await db;
+    await dbClient?.insert('cart', cart.toMap());
+    return cart;
+  }
+}
