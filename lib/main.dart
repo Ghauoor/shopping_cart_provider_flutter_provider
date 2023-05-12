de928874592d006047ea745b3c341bc673607386
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-05-12 18:30:42 +0500

    Cart Provider with SQL is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_cart_sql/model/provider/cart_provider.dart';
 import 'screens/product_list_screen.dart';
 
 void main() {
@@ -10,13 +12,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Cart App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: ProductListScreen(key: key),
+    return ChangeNotifierProvider(
+      create: (_) => CartProvider(),
+      child: Builder(builder: (BuildContext context) {
+        return MaterialApp(
+          title: 'Flutter Cart App',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.deepPurple,
+          ),
+          home: ProductListScreen(key: key),
+        );
+      }),
     );
   }
 }
---
+++ lib/model/provider/cart_provider.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CartProvider with ChangeNotifier {
+  int _counter = 0;
+
+  int get counter => _counter;
+
+  double _totalPrice = 0.0;
+  double get totalPrice => _totalPrice;
+
+  void _setPrefItem() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setInt('cart_item', _counter);
+    pref.setDouble('total_price', _totalPrice);
+
+    notifyListeners();
+  }
+
+  void _getPrefItem() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    _counter = pref.getInt('cart_item') ?? 0;
+    _totalPrice = pref.getDouble('total_price') ?? 0.0;
+    notifyListeners();
+  }
+
+  void addTotalPrice(double productPrice) {
+    _totalPrice += productPrice;
+    _setPrefItem();
+    notifyListeners();
+  }
+
+  void removeTotalPrice(double productPrice) {
+    _totalPrice -= productPrice;
+    _setPrefItem();
+    notifyListeners();
+  }
+
+  double getTotalPrice() {
+    _getPrefItem();
+    return _totalPrice;
+  }
+
+  void addCounter() {
+    _counter++;
+    _setPrefItem();
+    notifyListeners();
+  }
+
+  void removeCounter() {
+    _counter--;
+    _setPrefItem();
+    notifyListeners();
+  }
+
+  int getCounter() {
+    _getPrefItem();
+    return _counter;
+  }
+}
